12e56cdffc0ed62751cd783981eead2a13f36d87
treww <rykovanov.as@gmail.com>
2013-03-29 08:21:48 +0400

    separated common interfaces will be used both in client and server

---
+++ ua/server/include/opc/ua/computer.h
@@ -0,0 +1,53 @@
+/// @author Alexander Rykovanov 2013
+/// @email rykovanov.as@gmail.com
+/// @brief Opc Ua computer interface.
+/// @license GNU LGPL
+///
+/// Distributed under the GNU LGPL License
+/// (See accompanying file LICENSE or copy at 
+/// http://www.gnu.org/licenses/lgpl.html)
+///
+
+#ifndef OPC_UA_CLIENT_COMPUTER_H
+#define OPC_UA_CLIENT_COMPUTER_H
+
+#include <opc/ua/client/attributes.h>
+#include <opc/ua/client/endpoints.h>
+#include <opc/ua/client/view.h>
+
+#include <memory>
+#include <vector>
+
+namespace OpcUa
+{
+  namespace Remote
+  {
+
+    struct SessionParameters
+    {
+      ApplicationDescription ClientDescription;
+      std::vector<uint8_t> ClientCertificate;
+      std::string SessionName;
+      std::string ServerURI;
+      std::string EndpointURL;
+      Duration Timeout;
+    };
+
+
+
+    class Computer
+    {
+    public:
+      virtual void CreateSession(const SessionParameters& parameters) = 0;
+      virtual void ActivateSession() = 0;
+      virtual void CloseSession() = 0;
+
+     virtual std::shared_ptr<EndpointServices> Endpoints() const = 0;
+     virtual std::shared_ptr<ViewServices> Views() const = 0;
+     virtual std::shared_ptr<AttributeServices> Attributes() const = 0;
+    };
+
+  }
+}
+
+#endif //  OPC_UA_CLIENT_COMPUTER_H
