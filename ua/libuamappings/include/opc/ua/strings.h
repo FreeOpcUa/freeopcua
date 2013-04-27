4ac94b33b75e470564f1759c64f53223ba43990b
treww <rykovanov.as@gmail.com>
2013-04-28 01:59:15 +0400

    Added file with common browse name strings

---
+++ ua/libuamappings/include/opc/ua/strings.h
@@ -0,0 +1,27 @@
+/// @author Alexander Rykovanov 2013
+/// @email rykovanov.as@gmail.com
+/// @brief Well known node browse names.
+/// @license GNU LGPL
+///
+/// Distributed under the GNU LGPL License
+/// (See accompanying file LICENSE or copy at 
+/// http://www.gnu.org/licenses/lgpl.html)
+///
+
+#ifndef OPC_UA_STRINGS_H
+#define OPC_UA_STRINGS_H
+
+namespace OpcUa
+{
+  namespace Names
+  {
+
+    const char Objects[] = "Objects";
+    const char Root[]    = "Root";
+    const char Types[]   = "Types";
+    const char Views[]   = "Views";
+
+  }
+}
+
+#endif OPC_UA_STRINGS_H
