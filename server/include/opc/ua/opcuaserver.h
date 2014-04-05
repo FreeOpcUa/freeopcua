3ad4082d0fbaabf2ee89978bbe4191c8832e7d3c
Olivier R-D <olivier@dummy.com>
2014-04-05 14:49:36 +0200

    Add draft high level server object

---
+++ server/include/opc/ua/opcuaserver.h
@@ -0,0 +1,51 @@
+#ifndef OPCUASERVER_H
+#define OPCUASERVER_H
+
+#include<string>
+
+#include <opc/ua/server/addons/services_registry.h>
+#include <opc/common/addons_core/config_file.h>
+#include <opc/common/application.h>
+#include <opc/ua/computer.h>
+#include <opc/ua/node.h>
+
+
+namespace OpcUa
+{
+  class OPCUAServer
+  {
+    public:
+      OPCUAServer(); 
+      void SetEndpoint(const std::string endpoint){this->endpoint = endpoint;}
+      void SetURI(const std::string uri){this->uri = uri;}
+      void SetServerName(const std::string name){this->name = name;}
+      void AddAddressSpace(const std::string path) {xml_address_spaces.push_back(path);}
+
+      void Start();
+      void Stop();
+      //Node GetNode(std::vector<QualifiedName> browsepath); //Do we need that? or should we use rootnode anyway?
+      //Node GetNode(std::vector<std::string> browsepath);
+      Node GetRootNode();
+      void GetObjectsNode();
+      void SetConfigFile(const std::string path);
+
+    private:
+
+      Common::ModulesConfiguration const GetConfig();
+
+      OpcUa::Application::UniquePtr application;
+      OpcUa::Server::ServicesRegistryAddon::SharedPtr registry; //Why do I need to keep a pointer to the registry? 
+      std::vector<Common::AddonInformation> infos; //why do I need to keep a pointer? otherwise I get coredump
+      OpcUa::Remote::Computer::SharedPtr server;
+      std::vector<std::string> xml_address_spaces;
+      std::string config_path = "";
+      std::string endpoint = "opc.tcp://localhost:4841"; //This is the expected address of an OPC-UA server on a machine
+      std::string uri = "opcua.treww.org";
+      std::string name = "Open Source OPC-UA Server";
+      std::string security_mode = "none"; //should be correct class
+      
+  };
+
+}
+
+#endif
---
+++ server/src/opcuaserver.cpp
@@ -0,0 +1,125 @@
+#include <algorithm>
+#include <opc/common/application.h>
+#include <opc/common/addons_core/config_file.h>
+
+#include "daemon.h"
+
+#include <opc/ua/server/addons/services_registry.h>
+#include <opc/ua/server/addons/tcp_server_addon.h>
+#include <opc/ua/server/tcp_server.h>
+#include <opc/ua/server/addons/address_space_registry.h>
+#include <opc/ua/protocol/nodeid.h>
+#include <opc/ua/node.h>
+
+#include "opcuaserver.h"
+
+
+namespace OpcUa
+{
+  void OPCUAServer::SetConfigFile(const std::string path)
+  {
+    config_path = path;
+  }
+
+  Common::ModulesConfiguration const OPCUAServer::GetConfig()
+  {
+    if (config_path.length() > 0)
+    {
+      return Common::ParseConfiguration(config_path);
+    }
+    else
+    {
+
+      Common::ModulesConfiguration modules;
+
+      Common::ModuleConfiguration mod1;
+      mod1.ID = "services_registry";
+      mod1.Path = "libservices_registry_addon.so";
+      modules.push_back(mod1);
+
+      Common::ModuleConfiguration mod2;
+      mod2.ID = "tcp_server";
+      mod2.Path = "libopcua_tcp_server.so";
+      modules.push_back(mod2);
+
+      Common::ModuleConfiguration mod3;
+      mod3.ID = "endpoints_services";
+      mod3.Path = "libendpoints_services_addon.so";
+      mod3.Dependencies.push_back("services_registry");
+      modules.push_back(mod3);
+
+      Common::ModuleConfiguration mod4;
+      mod4.ID = "address_space_registry";
+      mod4.Path = "libaddress_space_addon.so";
+      mod4.Dependencies.push_back("services_registry");
+      modules.push_back(mod4);
+
+      Common::ModuleConfiguration mod5;
+      mod5.ID = "opcua_protocol";
+      mod5.Path = "libopcua_protocol.so";
+      mod5.Dependencies.push_back("tcp_server");
+      mod5.Dependencies.push_back("services_registry");
+      mod5.Dependencies.push_back("endpoints_services");
+      mod5.Parameters.Parameters.push_back(Common::Parameter("debug", "1"));
+      Common::ParametersGroup app("application");
+      app.Parameters.push_back(Common::Parameter("name", name));
+      app.Parameters.push_back(Common::Parameter("uri", uri));
+      app.Parameters.push_back(Common::Parameter("type", "server"));
+      Common::ParametersGroup ep("endpoint");
+      ep.Parameters.push_back(Common::Parameter("url", endpoint));
+      ep.Parameters.push_back(Common::Parameter("security_mode", security_mode));
+      ep.Parameters.push_back(Common::Parameter("security_policy_uri", "http://opcfoundation.org/UA/SecurityPolicy#None"));
+      ep.Parameters.push_back(Common::Parameter("transport_profile_uri", "http://opcfoundation.org/UA-Profile/Transport/uatcp-uasc-uabinary"));
+      //Common::ParametersGroup sec("user_token_policy");
+      //sec.Parameters.push_back(Common::Parameter("id", "anonymous"));
+      //sec.Parameters.push_back(Common::Parameter("type", "anonymous"));
+      //sec.Parameters.push_back(Common::Parameter("uri", "http://opcfoundation.org/UA/SecurityPolicy#None"));
+      //ep.Groups.push_back(sec);
+      app.Groups.push_back(ep);
+      mod5.Parameters.Groups.push_back(app);
+
+      modules.push_back(mod5);
+      return modules;
+    }
+  }
+
+  OPCUAServer::OPCUAServer()
+  {
+    application = OpcUa::CreateApplication();
+  }
+
+  void OPCUAServer::Start()
+  {
+    const Common::ModulesConfiguration modules = GetConfig(); 
+
+    //std::vector<Common::AddonInformation> infos; 
+    std::transform(modules.begin(), modules.end(), std::back_inserter(infos), std::bind(&Common::GetAddonInfomation, std::placeholders::_1));
+
+
+    application->Start(infos);
+
+    const Common::AddonsManager& addons = application->GetAddonsManager();
+    registry = addons.GetAddon<OpcUa::Server::ServicesRegistryAddon>(OpcUa::Server::ServicesRegistryAddonID);
+    server = registry->GetComputer();
+    //OpcUa::Server::TcpServerAddon::SharedPtr tcpserv = addons.GetAddon<OpcUa::Server::TcpServerAddon>(OpcUa::Server::TcpServerAddonID);
+    //tcpserv->;
+    //tcpserv->operator=
+    
+
+
+
+  }
+
+  void OPCUAServer::Stop()
+  {
+    registry.reset(); //for some reason the pointers must be reset before stopping the application
+    server.reset();
+    std::cout << "Stopping Application" << std::endl;
+    application->Stop();
+  }
+
+  Node OPCUAServer::GetRootNode()
+  {
+    return Node(server, OpcUa::ObjectID::RootFolder);
+  }
+}
