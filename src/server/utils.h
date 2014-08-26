1c84db1da2afa5154e9aa1cc98d533e7885829b9
Alexander Rykovanov <rykovanov.as@gmail.com>
2014-08-26 23:38:13 +0400

    Added fogotten files.

---
+++ src/server/utils.h
@@ -0,0 +1,56 @@
+/******************************************************************************
+ *   Copyright (C) 2013-2014 by Alexander Rykovanov                        *
+ *   rykovanov.as@gmail.com                                                   *
+ *                                                                            *
+ *   This library is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation; version 3 of the License.     *
+ *                                                                            *
+ *   This library is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU Lesser General Public License for more details.                      *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the                            *
+ *   Free Software Foundation, Inc.,                                          *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                *
+ ******************************************************************************/
+
+#pragma once
+
+
+#include <iostream>
+#include <vector>
+
+
+namespace OpcUa
+{
+  namespace UaServer
+  {
+
+    inline void PrintBlob(const std::vector<char>& buf)
+    {
+      unsigned pos = 0;
+      std::cout << "length: " << buf.size() << std::endl;
+      for (const auto it : buf)
+      {
+        if (pos)
+          printf((pos % 16 == 0) ? "\n" : " ");
+
+        printf("%02x", (unsigned)it & 0x000000FF);
+
+        if (it > ' ')
+          std::cout << "(" << it << ")";
+        else
+          std::cout << "   ";
+
+        ++pos;
+      }
+
+      std::cout << std::endl << std::flush;
+    }
+
+
+  }
+}
