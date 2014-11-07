10fb521aab572e6064ec2f00060d032179fd0a28
Matthieu Bec <mbec@gmto.org>
2014-11-07 12:27:39 -0800

    ToDateTime optional usec

--- tests/test_highlevel.py
+++ tests/test_highlevel.py
@@ -64,7 +64,7 @@ class Unit(unittest.TestCase):
         dv = opcua.DataValue('abc')
         self.assertEqual(dv.value, 'abc')
         tnow = long(time.time())
-        dv.source_timestamp=opcua.ToDateTime(tnow,0)
+        dv.source_timestamp=opcua.ToDateTime(tnow)
         self.assertEqual(opcua.ToTimeT(dv.source_timestamp), tnow)
         dv = opcua.DataValue(True,opcua.VariantType.BOOLEAN)
         self.assertEqual(dv.value,True)
