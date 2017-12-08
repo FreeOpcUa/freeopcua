e4172496a3c9692253cdc9efda4522b76162ec86
Fanda Vacek <vacek@elektroline.cz>
2017-12-08 14:47:15 +0100

    URI regexp explained

--- src/core/common/uri_facade_lin.cpp
+++ src/core/common/uri_facade_lin.cpp
@@ -20,14 +20,38 @@ namespace Common
 
 void Uri::Initialize(const std::string &uriString)
 {
-  std::regex uri_regex("([a-zA-Z][a-zA-Z0-9.+-]*)://(([^@:]*)(:([^@]+))?@)?([^/:]+)(:([0-9]+))?([^?]*)(.*)?");
+  /*
+   acording to wiki https://en.wikipedia.org/wiki/Uniform_Resource_Identifier#URI_resolution
+
+   The scheme, consisting of a sequence of characters beginning with a letter and followed by any combination of letters, digits, plus (+), period (.), or hyphen (-)
+   ([a-zA-Z][a-zA-Z0-9.+-]*)
+
+   An optional authentication section of a user name and password, separated by a colon, followed by an at symbol (@)
+   (([^@:]*)(:([^@]+))?@)?
+
+   A host, consisting of either a registered name (including but not limited to a hostname), or an IP address.
+   IPv4 addresses must be in dot-decimal notation, and IPv6 addresses must be enclosed in brackets ([ ])
+   ([^/:?]+)
+
+   An optional port number, separated from the hostname by a colon
+   (:([0-9]+))?
+
+   A path, which contains data, usually organized in hierarchical form, that appears as a sequence of segments separated by slashes.
+   Such a sequence may resemble or map exactly to a file system path, but does not always imply a relation to one.
+   The path must begin with a single slash (/) if an authority part was present
+   (/[^?#]*)?
+
+   rest of URI is query + fragment, currently not used in freeopcua
+   (.*)
+   */
+  std::regex uri_regex("([a-zA-Z][a-zA-Z0-9.+-]*)://(([^@:]*)(:([^@]+))?@)?([^/:?]+)(:([0-9]+))?(/[^?#]*)?(.*)");
   std::smatch uri_match;
   if (!std::regex_match(uriString, uri_match, uri_regex))
     {
       THROW_ERROR1(CannotParseUri, uriString);
     }
 
-  enum {Scheme = 1, User = 3, Password = 5, Host = 6, Port = 8, Path = 9, Query = 10, Fragment = 11};
+  enum {Scheme = 1, User = 3, Password = 5, Host = 6, Port = 8, Path = 9, QueryAndFragment = 10};
 
   SchemeStr = uri_match[Scheme].str();
   PasswordStr = uri_match[Password].str();
